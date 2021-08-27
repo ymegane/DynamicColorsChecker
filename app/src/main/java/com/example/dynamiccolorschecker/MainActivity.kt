508e582beb76ab3b150de1a58b0ff5d02a987c4e
Yoshitake Furuie <y.furuie@goodpatch.com>
2021-08-27 13:29:25 +0900

    draw colors

--- app/src/main/java/com/example/dynamiccolorschecker/MainActivity.kt
+++ app/src/main/java/com/example/dynamiccolorschecker/MainActivity.kt
@@ -3,14 +3,23 @@ package com.example.dynamiccolorschecker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.dynamiccolorschecker.ui.theme.DynamicColorsCheckerTheme
+import androidx.compose.ui.unit.dp
+import com.example.dynamiccolorschecker.ui.theme.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,22 +31,61 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    ColorBoxes()
                 }
             }
         }
     }
 }
 
+val groupedItems = mapOf(
+    "NEUTRAL1" to NEUTRAL1,
+    "NEUTRAL2" to NEUTRAL2,
+    "ACCENT1" to ACCENT1,
+    "ACCENT2" to ACCENT2,
+)
+
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ColorBoxes() {
+    LazyColumn {
+        groupedItems.forEach { (category, items) ->
+            stickyHeader {
+                Header(category = category)
+            }
+
+            items(items) {item ->
+                ColorBox(item = item)
+            }
+        }
+    }
+}
+
+@Composable
+fun Header(category : String) {
+    Box(modifier = Modifier
+        .background(Color.DarkGray)
+        .fillMaxWidth()
+        .padding(8.dp)) {
+        Text(text = category, color = Color.White, style = TextStyle(fontWeight = FontWeight.Bold))
+    }
+}
+
+@Composable
+fun ColorBox(item: ColorMap) {
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .height(60.dp)
+            .background(colorResource(id = item.res))) {
+        Text(text = item.name)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     DynamicColorsCheckerTheme {
-        Greeting("Android")
+        ColorBoxes()
     }
 }
