aa74754c6407c30eec5cbd1bd2f1eca7688f4296
Yoshitake Furuie <y.furuie@goodpatch.com>
2021-08-28 17:28:33 +0900

    Show color code

--- app/src/main/java/com/example/dynamiccolorschecker/MainActivity.kt
+++ app/src/main/java/com/example/dynamiccolorschecker/MainActivity.kt
@@ -12,15 +12,19 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.toUpperCase
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.dynamiccolorschecker.ui.theme.*
+import java.util.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -74,14 +78,27 @@ fun Header(category : String) {
 
 @Composable
 fun ColorBox(item: ColorMap) {
+    val color = colorResource(id = item.res)
     Row(
         Modifier
             .fillMaxWidth()
             .height(60.dp)
-            .background(colorResource(id = item.res))
+            .background(color)
             .padding(8.dp)) {
-        Text(text = item.name, color = Color.White, style = TextStyle(shadow = Shadow(color = Color.DarkGray, blurRadius = 4F)))
+        Column(Modifier.fillMaxHeight(), verticalArrangement = Arrangement.SpaceBetween) {
+            ItemText(text = item.name)
+            ItemText(text = convertToHexString(color))
+        }
     }
+}
+
+@Composable
+fun ItemText(text: String) {
+    Text(text = text, color = Color.White, style = TextStyle(shadow = Shadow(color = Color.DarkGray, blurRadius = 4F)))
+}
+
+private fun convertToHexString(color: Color): String {
+    return  String.format("#%x", color.toArgb()).uppercase()
 }
 
 @Preview(showBackground = true)
